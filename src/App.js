40e933155a3b6060c31de49c3f13571c81a3308c
Mumina033 <mumina.mohamed033@gmail.com>
2024-07-22 17:22:14 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>{" "}
-      <footer>
-        This project was created by{" "}
-        <a
-          href="https://github.com/Mumina033"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Mumina Mohamed
-        </a>{" "}
-        and is
-        <a
-          href="https://github.com/Mumina033/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>{" "}
+        <footer>
+          This project was created by{" "}
+          <a
+            href="https://github.com/Mumina033"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mumina Mohamed
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/Mumina033/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
