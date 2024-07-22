e6e3c2a16fa0a809e371fb65f90608c27e47c61e
Mumina033 <mumina.mohamed033@gmail.com>
2024-07-22 17:10:20 -0400

    included my GitHub link  within the footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>{" "}
+      <footer>
+        This project was created by{" "}
+        <a
+          href="https://github.com/Mumina033"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mumina Mohamed
+        </a>{" "}
+        and is
+        <a
+          href="https://github.com/Mumina033/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
